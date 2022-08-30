5fffbd9e9e6fe8d2c3046a9e30fa4b52af55a298
david082321 <7046617+david082321@users.noreply.github.com>
2022-08-30 22:34:07 +0800

    Update TtsStyleManger.java

--- app/src/main/java/me/ag2s/tts/services/TtsStyleManger.java
+++ app/src/main/java/me/ag2s/tts/services/TtsStyleManger.java
@@ -29,33 +29,33 @@ public class TtsStyleManger {
 
     private TtsStyleManger() {
         styles = new ArrayList<>(22);
-        styles.add(new TtsStyle("默认", "Default", "默认值"));
-        styles.add(new TtsStyle("新闻", "newscast", "以正式专业的语气叙述新闻"));
-        styles.add(new TtsStyle("客服", "customerservice", "以友好热情的语气为客户提供支持"));
-        styles.add(new TtsStyle("助理", "assistant", "以热情而轻松的语气对数字助理讲话"));
-        styles.add(new TtsStyle("聊天", "chat", "以轻松、随意的语气闲聊"));
-        styles.add(new TtsStyle("冷静", "calm", "以沉着冷静的态度说话。 语气、音调、韵律与其他语音类型相比要统一得多。"));//Calm
-        styles.add(new TtsStyle("开心", "cheerful", "以较高的音调和音量表达欢快、热情的语气"));//Cheerful Happy
-        styles.add(new TtsStyle("悲伤", "sad", "以较高的音调、较低的强度和较低的音量表达悲伤的语气。 这种情绪的常见特征是说话时呜咽或哭泣。"));//Sad
-        styles.add(new TtsStyle("恼怒", "angry", "以较低的音调、较高的强度和较高的音量来表达恼怒的语气。 说话者处于愤怒、生气和被冒犯的状态。")); //Angry
-        styles.add(new TtsStyle("恐惧", "fearful", "以较高的音调、较高的音量和较快的语速来表达恐惧、紧张的语气。 说话者处于紧张和不安的状态。"));//Fearful,Nervous
-        styles.add(new TtsStyle("抱怨", "disgruntled", "表达轻蔑和抱怨的语气。 这种情绪的语音表现出不悦和蔑视。"));// Disgruntled Complaining
-        styles.add(new TtsStyle("严肃", "serious", "表达严肃和命令的语气。 说话者的声音通常比较僵硬，节奏也不那么轻松。"));//Serious Strict
-        styles.add(new TtsStyle("撒娇", "affectionate", "以较高的音调和音量表达温暖而亲切的语气。 说话者处于吸引听众注意力的状态。 说话者的“个性”往往是讨人喜欢的。"));//Affectionate Cutesy
-        styles.add(new TtsStyle("温柔", "gentle", "以较低的音调和音量表达温和、礼貌和愉快的语气"));//Gentle
-        styles.add(new TtsStyle("抒情", "lyrical", "以优美又带感伤的方式表达情感"));
-        styles.add(new TtsStyle("尴尬", "embarrassed", "在说话者感到不舒适时表达不确定、犹豫的语气"));//{\"Name\":\"Embarrassed\",\"LegacyNames\":[\"Shy\"]}
-        styles.add(new TtsStyle("同理心", "empathetic", "表达出一种关怀和理解的感觉。"));
-        styles.add(new TtsStyle("沮丧", "depressed", "调低音调和音量来表达忧郁、沮丧的语气"));//Depressed
+        styles.add(new TtsStyle("預設", "Default", "預設值"));
+        styles.add(new TtsStyle("新聞", "newscast", "以正式專業的語氣敘述新聞"));
+        styles.add(new TtsStyle("客服", "customerservice", "以友好熱情的語氣為客戶提供支援"));
+        styles.add(new TtsStyle("助理", "assistant", "以熱情而輕鬆的語氣對數字助理講話"));
+        styles.add(new TtsStyle("聊天", "chat", "以輕鬆、隨意的語氣閒聊"));
+        styles.add(new TtsStyle("冷靜", "calm", "以沉著冷靜的態度說話。 語氣、音調、韻律與其他語音類型相比要統一得多。"));//Calm
+        styles.add(new TtsStyle("開心", "cheerful", "以較高的音調和音量表達歡快、熱情的語氣"));//Cheerful Happy
+        styles.add(new TtsStyle("悲傷", "sad", "以較高的音調、較低的強度和較低的音量表達悲傷的語氣。 這種情緒的常見特徵是說話時嗚咽或哭泣。"));//Sad
+        styles.add(new TtsStyle("惱怒", "angry", "以較低的音調、較高的強度和較高的音量來表達惱怒的語氣。 說話者處於憤怒、生氣和被冒犯的狀態。")); //Angry
+        styles.add(new TtsStyle("恐懼", "fearful", "以較高的音調、較高的音量和較快的語速來表達恐懼、緊張的語氣。 說話者處於緊張和不安的狀態。"));//Fearful,Nervous
+        styles.add(new TtsStyle("抱怨", "disgruntled", "表達輕蔑和抱怨的語氣。 這種情緒的語音表現出不悅和蔑視。"));// Disgruntled Complaining
+        styles.add(new TtsStyle("嚴肅", "serious", "表達嚴肅和指令的語氣。 說話者的聲音通常比較僵硬，節奏也不那麼輕鬆。"));//Serious Strict
+        styles.add(new TtsStyle("撒嬌", "affectionate", "以較高的音調和音量表達溫暖而親切的語氣。 說話者處於吸引聽眾注意力的狀態。 說話者的「個性」往往是討人喜歡的。"));//Affectionate Cutesy
+        styles.add(new TtsStyle("溫柔", "gentle", "以較低的音調和音量表達溫和、禮貌和愉快的語氣"));//Gentle
+        styles.add(new TtsStyle("抒情", "lyrical", "以優美又帶感傷的方式表達情感"));
+        styles.add(new TtsStyle("尷尬", "embarrassed", "在說話者感到不舒適時表達不確定、猶豫的語氣"));//{\"Name\":\"Embarrassed\",\"LegacyNames\":[\"Shy\"]}
+        styles.add(new TtsStyle("同理心", "empathetic", "表達出一種關懷和理解的感覺。"));
+        styles.add(new TtsStyle("沮喪", "depressed", "調低音調和音量來表達憂鬱、沮喪的語氣"));//Depressed
 
-        styles.add(new TtsStyle("新闻(休闲）", "newscast-casual", "以通用和随意的语气叙述新闻。"));
-        styles.add(new TtsStyle("新闻(正式）", "newscast-formal", "以正式、自信、权威的语气叙述新闻。"));
-        styles.add(new TtsStyle("旁白-专业", "narration-professional", "对内容阅读表达专业、客观的语气。（云扬，Aria）"));
-        styles.add(new TtsStyle("轻松阅读", "narration-relaxed", "适合阅读轻松的文章(云希，云健)"));
+        styles.add(new TtsStyle("新聞(休閒）", "newscast-casual", "以通用和隨意的語氣敘述新聞。"));
+        styles.add(new TtsStyle("新聞(正式）", "newscast-formal", "以正式、自信、權威的語氣敘述新聞。"));
+        styles.add(new TtsStyle("旁白-專業", "narration-professional", "對內容閱讀表達專業、客觀的語氣。（雲揚，Aria）"));
+        styles.add(new TtsStyle("輕鬆閱讀", "narration-relaxed", "適合閱讀輕鬆的文章(雲希，雲健)"));
 
-        styles.add(new TtsStyle("体育解说", "Sports_commentary", "体育解说(云健)"));
-        styles.add(new TtsStyle("体育解说(兴奋)", "Sports_commentary_excited", "兴奋语气的体育解说(云健)"));
-        styles.add(new TtsStyle("乐观的广告", "Advertisement_upbeat", "(云皓)Advertisement_upbeat"));
+        styles.add(new TtsStyle("體育解說", "Sports_commentary", "體育解說(雲健)"));
+        styles.add(new TtsStyle("體育解說(興奮)", "Sports_commentary_excited", "興奮語氣的體育解說(雲健)"));
+        styles.add(new TtsStyle("樂觀的廣告", "Advertisement_upbeat", "(雲皓)Advertisement_upbeat"));
 
 
         Log.e("Style", styles.size() + "");
@@ -74,7 +74,7 @@ public class TtsStyleManger {
         if (index >= 0 && index < styles.size()) {
             return styles.get(index);
         }
-        return new TtsStyle("默认", "", "默认值");
+        return new TtsStyle("預設", "", "預設值");
     }
 
 }
